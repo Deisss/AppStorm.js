73e852033a1ac266c3206c76b8c8ec60b60b5186
Deisss <villette.charles@gmail.com>
2013-10-20 02:03:53 +0200

    rework parser content.

--- appstorm/core/parser.js
+++ appstorm/core/parser.js
@@ -1,134 +1,174 @@
 /* ************************************************************************
 
-	License: MIT Licence
+    License: MIT Licence
 
-	Authors: VILLETTE Charles
+    Authors: VILLETTE Charles
 
-	Date: 2013-05-10
+    Date: 2013-05-10
 
-	Date of last modification: 2013-10-11
+    Date of last modification: 2013-10-20
 
-	Dependencies : [
-		a.js
-	]
+    Dependencies : [
+        a.js
+    ]
 
-	Events : []
+    Events : []
 
-	Description:
-		Provide parsing functionnality for using json, xml, html...
+    Description:
+        Provide parsing/stringify functionnality for JSON and XML format
 
 ************************************************************************ */
 
-
-// provide parsing functionality for using json, xml, html...
+// Provide parsing/stringify functionnality for JSON and XML format
 a.parser = {
-	/**
-	 * Basic JSON handler wich prevent from "no data" or "wrong data" input, with a log message to check
-	 *
-	 * Examples: <a href="http://appstormjs.com/wiki/doku.php?id=appstorm.js_v0.1:core:parser">here</a>
-	 *
-	 * @class json
-	 * @static
-	 * @namespace a.parser
-	*/
-	json : {
-		/**
-		 * Serialize a JSON into a string
-		 *
-		 * @method stringify
-		 *
-		 * @param value {Mixed} Any data to be converted into String
-		 * @return {String} A parsed string, or an empty string if the parsing fails
-		*/
-		stringify : function(value) {
-			try {
-				return JSON.stringify(value);
-			} catch(e) {
-				a.console.error("a.parser.json.stringify : unable to stringify the value : " + value, 1);
-				return "";
-			}
-		},
+    /**
+     * Basic JSON handler wich prevent from 'no data' or 'wrong data' input,
+     * with a log message to check
+     *
+     * Examples: <a href="http://appstormjs.com/wiki/doku.php?id=appstorm.js_v0.1:core:parser">here</a>
+     *
+     * @class json
+     * @static
+     * @namespace a.parser
+    */
+    json: {
+        /**
+         * Serialize a JSON into a string
+         *
+         * @method stringify
+         *
+         * @param value {Object}         Any data to be converted into String
+         * @return {String}              A JSON parsed string, or an empty
+         *                               string if the parsing fails
+        */
+        stringify: function(value) {
+            try {
+                return JSON.stringify(value);
+            } catch(e) {
+                var unable = 'a.parser.json.stringify: ' +
+                            'unable to stringify (value: ' + value + ')';
+                a.console.error(unable, 1);
+                // Debug stack trace in case of debug mode
+                if(a.environment.get('debug')) {
+                    a.console.error(a.getStackTrace(), 1);
+                }
+                return '';
+            }
+        },
 
-		/**
-		 * Deserialize a string into JSON
-		 *
-		 * @method parse
-		 *
-		 * @param value {String} The value un-stringify
-		 * @return {Mixed | null} The converted value
-		*/
-		parse : function(value) {
-			try {
-				return JSON.parse(value);
-			} catch(e) {
-				a.console.error("a.parser.json.parse : unable to parse the value : " + value, 1);
-				return null;
-			}
-		}
-	},
+        /**
+         * Deserialize a string into JSON
+         *
+         * @method parse
+         *
+         * @param value {String}            The value un-stringify
+         * @return {Mixed | null}           The converted value
+        */
+        parse: function(value) {
+            try {
+                return JSON.parse(value);
+            } catch(e) {
+                var unable = 'a.parser.json.parse: ' +
+                            'unable to parse (value: ' + value + ')';
+                a.console.error(unable, 1);
+                // Debug stack trace in case of debug mode
+                if(a.environment.get('debug')) {
+                    a.console.error(a.getStackTrace(), 1);
+                }
+                return null;
+            }
+        }
+    },
 
-	/**
-	 * Basic XML handler wich prevent from "no data" or "wrong data" input, with a log message to check
-	 *
-	 * Examples: <a href="http://appstormjs.com/wiki/doku.php?id=appstorm.js_v0.1:core:parser">here</a>
-	 *
-	 * @class xml
-	 * @static
-	 * @namespace a.parser
-	*/
-	xml : {
-		/**
-		 * Serialize a XML into a string
-		 *
-		 * @method stringify
-		 *
-		 * @param value {Mixed} Any data to be converted into String
-		 * @return {String} A parsed string, or an empty string if the parsing fails
-		*/
-		stringify : function(value) {
-			if(!a.isNull(value) && !a.isNull(value.xml)) {
-				return value.xml;
-			} else if(!a.isNull(window.XMLSerializer)) {
-				try {
-					return (new window.XMLSerializer()).serializeToString(value);
-				} catch(e) {
-					a.console.error("a.parser.xml.stringify : unable to stringify the value : " + value, 1);
-					return "";
-				}
-			}
-			a.console.error("a.parser.xml.stringify : unable to find any parser available", 1);
-			return "";
-		},
+    /**
+     * Basic XML handler wich prevent from 'no data' or 'wrong data' input,
+     * with a log message to check
+     *
+     * Examples: <a href="http://appstormjs.com/wiki/doku.php?id=appstorm.js_v0.1:core:parser">here</a>
+     *
+     * @class xml
+     * @static
+     * @namespace a.parser
+    */
+    xml: {
+        /**
+         * Serialize a XML into a string
+         *
+         * @method stringify
+         *
+         * @param value {Object}      Any data to be converted into String
+         * @return {String}           A parsed string, or an empty
+         *                            string if the parsing fails
+        */
+        stringify: function(value) {
+            if(!a.isNull(value) && !a.isNull(value.xml)) {
+                return value.xml;
+            } else if(!a.isNull(window.XMLSerializer)) {
+                try {
+                    var serializer = new window.XMLSerializer();
+                    return serializer.serializeToString(value);
+                } catch(e) {
+                    var unable = 'a.parser.xml.stringify: ' +
+                                 'unable to stringify (value: ' + value + ')';
+                    a.console.error(unable, 1);
+                    // Debug stack trace in case of debug mode
+                    if(a.environment.get('debug')) {
+                        a.console.error(a.getStackTrace(), 1);
+                    }
+                } finally {
+                    return '';
+                }
+            }
 
-		/**
-		 * Deserialize a string into XML
-		 *
-		 * @method parse
-		 *
-		 * @param value {String} The value un-stringify
-		 * @return {DOCElement | null} The resulting doc element (null in case of problem)
-		*/
-		parse : function(value) {
-			if(!a.isNull(window.ActiveXObject)) {
-				var doc = null;
-				var msxml = ["Msxml2.DOMDocument.6.0", "Msxml2.DOMDocument.5.0", "Msxml2.DOMDocument.4.0", "MSXML2.DOMDocument.3.0"];
-				for(var i=0, l=msxml.length; i<l; ++i) {
-					try {
-						doc = new ActiveXObject(msxml[i]);
-					} catch(e) {}
-				}
-				doc.async = false;
-				doc.loadXML(value);
-				if (doc.parseError.errorCode != 0) {
-					a.console.error("a.parser.xml.parse : error while parsing content : " + doc.parseError.reason, 1);
-					return null;
-				}
-				return doc;
-			} else if(!a.isNull(window.DOMParser)) {
-				return (new DOMParser()).parseFromString(value, "text/xml");
-			}
-			a.console.error("a.parser.xml.parse : unable to find any parser available", 1);
-			return null;
-		}
-	}
+            var noParserFound = 'a.parser.xml.stringify: ' +
+                                'unable to find any parser available';
+            a.console.error(noParserFound, 1);
+            return '';
+        },
+
+        /**
+         * Deserialize a string into XML
+         *
+         * @method parse
+         *
+         * @param value {String}          The value un-stringify
+         * @return {DOMElement | null}    The resulting doc element, or null
+         *                                in case of problem
+        */
+        parse: function(value) {
+            if(!a.isNull(window.ActiveXObject)) {
+                var doc = null;
+                // 4: we stop at MSXML 3.0
+                for(var i=0; i<4; ++i) {
+                    try {
+                        // Name are: Msxml2.DOMDocument.6.0 to 3.0
+                        var msxml = 'MSXML2.DOMDocument.' + (6 - i) + '.0';
+                        doc = new ActiveXObject(msxml[i]);
+                    } catch(e) {}
+                }
+                doc.async = false;
+                doc.loadXML(value);
+                if (doc.parseError.errorCode != 0) {
+                    var unable = 'a.parser.xml.parse: ' +
+                                 'unable to parse (value: ' + value +
+                                 ', reason' + doc.parseError.reason + ')';
+                    a.console.error(unable, 1);
+                    // Debug stack trace in case of debug mode
+                    if(a.environment.get('debug')) {
+                        a.console.error(a.getStackTrace(), 1);
+                    }
+
+                    return null;
+                }
+                return doc;
+            } else if(!a.isNull(window.DOMParser)) {
+                return (new DOMParser()).parseFromString(value, 'text/xml');
+            }
+
+            var noParserFound = 'a.parser.xml.parse: ' +
+                                'unable to find any parser available';
+            a.console.error(noParserFound, 1);
+            return null;
+        }
+    }
 };
