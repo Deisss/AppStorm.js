4ad9f46d188d45d0939dda681c76ffca099e4592
Deisss <charles.villette@gmail.com>
2013-12-30 01:37:56 +0100

    Remove the previous object to base everything on new a.mem object.

--- appstorm/core/environment.js
+++ appstorm/core/environment.js
@@ -4,22 +4,15 @@
 
     Dependencies: [
         a.js
-        core/message.js
+        core/mem.js
     ]
 
     Events: [
-        a.environment.add: {
-            key:   the environment key added/modified,
-            value: the value for this key
-        }
-        a.environment.remove: {
-            key:   the environment key removed
-        }
     ]
 
     Description:
         Environment functionnality, to get access to some basic
-        "main options" for system
+        'main options' for system
 
 ************************************************************************ */
 
@@ -35,80 +28,8 @@
  * @static
  * @namespace a
 */
-a.environment = new function() {
-    /*
-     * DON'T USE DIRECTLY
-     *
-     * Internal store
-    */
-    var store = {
-        'verbose': 2,
-        'console': 'log'
-    };
+a.environment = a.mem.getInstance('app.environment');
 
-    /**
-     * Get the stored value for given key, null if nothing is stored
-     *
-     * @method get
-     *
-     * @param key {String}     The key to retreive
-     * @return {Mixed | null} The result data, or null if key is not found
-    */
-    this.get = function(key) {
-        return (key in store) ? store[key] : null;
-    };
-
-    /**
-     * Store or modify the key data with incoming value
-     *
-     * @method set
-     *
-     * @param key {String}     The key to store
-     * @param value {Mixed}    Some data to associate to the key
-    */
-    this.set = function(key, value) {
-        if(a.isNone(key)) {
-            return;
-        }
-
-        store[key] = value;
-
-        // Dispatch event
-        a.message.dispatch('a.environment.add', {
-            key:   key,
-            value: value
-        });
-    };
-
-    /**
-     * Remove a key stored
-     *
-     * @method remove
-     *
-     * @param key {String} The stored key to remove
-    */
-    this.remove = function(key) {
-        if(a.isNone(store[key])) {
-            return;
-        }
-
-        delete store[key];
-
-        // Dispatch event
-        a.message.dispatch('a.environment.remove', {
-            key: key
-        });
-    };
-
-    /**
-     * Erase everything and rollback to inital change (verbose:2, console: log)
-     *
-     * @method clear
-    */
-    this.clear = function() {
-        store = {
-            'verbose': 2,
-            'console': 'log'
-        };
-    };
-};
+// Default data
+a.environment.set('verbose', 2);
+a.environment.set('console', 'log');
