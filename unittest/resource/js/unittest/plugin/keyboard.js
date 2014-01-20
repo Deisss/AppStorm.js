8200ea5b89c93e7e5ab133a0d3cea58b72e469dc
Deisss <charles.villette@gmail.com>
2014-01-21 00:53:00 +0100

    Finishing the keyboard unit test.

--- unittest/resource/js/unittest/plugin/keyboard.js
+++ unittest/resource/js/unittest/plugin/keyboard.js
@@ -1,53 +1,55 @@
 // Unit test for a.keyboard (plugin)
 
-module("PLUGIN/keyboard");
+module('plugin/keyboard.js');
 
 /*
- * Note : because keyboard plugin is just a bind to Mousetrap, we just do one basic test nothing more.
- * Note : please run unit test from Mousetrap for more deeper unit test (you can found them on /unittest/vendor/mousetrap)
+ * Note : because keyboard plugin is just a bind to Mousetrap,
+ *        we just do one basic test nothing more.
+ * Note : please run unit test from Mousetrap for more deeper unit test
+ *        (you can found them on /unittest/vendor/mousetrap)
 */
 
-test("a.keyboard.addListener", function() {
-	stop();
-	expect(1);
+test('a.keyboard.addListener', function() {
+    stop();
+    expect(1);
 
-	// Prevent scope change
-	var se = strictEqual,
-		st = start;
+    // Prevent scope change
+    var se = strictEqual,
+        st = start;
 
-	var callback = function() {
-		se(true, true, "Event where fired as expected");
-		st();
-	};
+    var callback = function() {
+        se(true, true, 'Event where fired as expected');
+        st();
+    };
 
-	a.keyboard.addListener("a", callback);
-	a.keyboard.addListener("c", callback);
+    a.keyboard.bind('a', callback);
+    a.keyboard.bind('c', callback);
 
-	// Now launching mousetrap trigger (only one of two callback should pass)
-	Mousetrap.trigger("a");
+    // Now launching mousetrap trigger (only one of two callback should pass)
+    Mousetrap.trigger('a');
 });
 
-test("a.keyboard.removeListener", function() {
-	stop();
-	expect(2);
+test('a.keyboard.removeListener', function() {
+    stop();
+    expect(2);
 
-	// Prevent scope change
-	var se = strictEqual;
+    // Prevent scope change
+    var se = strictEqual;
 
-	var callback = function() {
-		se(true, true, "Event where fired as expected");
-	};
+    var callback = function() {
+        se(true, true, 'Event where fired as expected');
+    };
 
-	a.keyboard.addListener("a", callback);
-	a.keyboard.addListener("c", callback);
+    a.keyboard.bind('a', callback);
+    a.keyboard.bind('c', callback);
 
-	// Now launching mousetrap trigger (only one of two callback should pass)
-	Mousetrap.trigger("a");
+    // Now launching mousetrap trigger (only one of two callback should pass)
+    Mousetrap.trigger('a');
 
-	a.keyboard.removeListener("a");
+    a.keyboard.unbind('a');
 
-	Mousetrap.trigger("a");
-	Mousetrap.trigger("c");
+    Mousetrap.trigger('a');
+    Mousetrap.trigger('c');
 
-	setTimeout(start, 1000);
+    setTimeout(start, 1000);
 });
