873597841e8ec5a294a670a039c423c0310f2e4e
Deisss <charles.villette@gmail.com>
2014-02-17 14:51:31 +0100

    Correct indent on mail example.

--- example/mail/resource/js/boot.js
+++ example/mail/resource/js/boot.js
@@ -1,33 +1,37 @@
-// This part allow system to startup (decide what to startup, in which situation)
-
+// This part allow system to startup
+// (decide what to startup, in which situation)
 (function() {
-	var currentHash = a.hash.getHash(),
-		timerId = null,
-		max = 1000;
+    var currentHash = a.hash.getHash(),
+        timerId = null,
+        max = 1000;
 
-	// Initialise page event hash system
-	a.hash.setPreviousHash("");
-	window.location.href = "#loading_application";
+    // Initialise page event hash system
+    a.hash.setPreviousHash('');
+    window.location.href = '#';
 
-	/**
-	 * handle "state change" for every browser
-	*/
-	function firstHandle() {
-		if(a.hash.getHash() !== currentHash) {
-			window.location.href = "#" + currentHash;
-			max = 0;
-		}
-		if(max-- <= 0) {
-			a.timer.remove(timerId);
-		}
-	};
+    /**
+     * handle "state change" for every browser
+     *
+     * @method firstHandle
+    */
+    function firstHandle() {
+        if(a.hash.getHash() !== currentHash) {
+            window.location.href = '#' + currentHash;
+            max = 0;
+        }
+        if(max-- <= 0) {
+            a.timer.remove(timerId);
+        }
+    };
 
-	// The main starter is here, we will customize it soon
-	if(currentHash === null || currentHash === "" || !a.state.hashExists(currentHash)) {
-		currentHash = "login";
-	}
+    // The main starter is here, we will customize it soon
+    if(currentHash === null || currentHash === '' ||
+        !a.state.hashExists(currentHash)) {
+        currentHash = 'login';
+    }
 
-	// Some browser don't get hash change with onHashchange event, so we decide to use timer
-	// Note : a.page.event.hash is more complex, here is little example
-	timerId = a.timer.add(firstHandle, null, 10);
+    // Some browser don't get hash change with onHashchange event,
+    // so we decide to use timer
+    // Note : a.page.event.hash is more complex, here is little example
+    timerId = a.timer.add(firstHandle, null, 10);
 })();
--- example/mail/resource/js/loader.js
+++ example/mail/resource/js/loader.js
@@ -1,228 +1,217 @@
 // Root controller system, loading main app system here
 (function() {
-	/**
-	 * Replace the content regarding the id given
-	 *
-	 * @param id {String} The id to replace inner html
-	*/
-	function __replaceById(id) {
-		return function(content) {
-			a.page.template.replace(document.getElementById(id), content);
-		}
-	};
+    // We got two main states : the login/password lost one,
+    // and the main page (after login)
+    var notLoggedState = {
+        id:    'root-notlogged',
+        entry: '#page-container',
+        type:  'replace',
 
-	// We got two main states : the login/password lost one, and the main page (after login)
-	var notLoggedState = {
-		id : "root-notlogged",
-		// We load base data : centered element to recieve login and password lost
-		include : {
-			css  : "resource/css/root-notlogged.css",
-			html : "resource/html/root-notlogged.html"
-		},
+        // We load base data : centered element to recieve login and
+        // password lost
+        include: {
+            css:  'resource/css/root-notlogged.css',
+            html: 'resource/html/root-notlogged.html'
+        },
 
-		// We define how to use html loaded (content is a DOM document)
-		load : __replaceById("page-container"),
+        // From 'not logged' state, we can access to 2 sub states:
+        // password lost page, and login page
+        children: [
+            {
+                id:    'login',
+                hash:  'login',
+                title: 'AppStorm.JS - login',
 
-		// From 'not logged' state, we can access to 2 sub states : password lost page, and login page
-		children : [
-			{
-				id    : "login",
-				hash  : "login",
-				title : "AppStorm.JS - login",
-				include : {
-					css  : "resource/css/root-notlogged-login.css",
-					html : "resource/html/root-notlogged-login.html"
-				},
+                entry: '#root-notlogged-content',
+                type:  'replace',
 
-				// We define how to use html loaded (content is a DOM document)
-				load : __replaceById("root-notlogged-content")
-			},
-			{
-				id    : "password-lost",
-				hash  : "password-lost",
-				title : "AppStorm.JS - password lost",
-				include : {
-					css  : "resource/css/root-notlogged-password-lost.css",
-					html : "resource/html/root-notlogged-password-lost.html"
-				},
+                include: {
+                    css:  'resource/css/root-notlogged-login.css',
+                    html: 'resource/html/root-notlogged-login.html'
+                }
+            },
+            {
+                id:    'password-lost',
+                hash:  'password-lost',
+                title: 'AppStorm.JS - password lost',
 
-				// We define how to use html loaded (content is a DOM document)
-				load :  __replaceById("root-notlogged-content")
-			}
-		]
-	};
+                entry: '#root-notlogged-content',
+                type:  'replace',
+
+                include: {
+                    css:  'resource/css/root-notlogged-password-lost.css',
+                    html: 'resource/html/root-notlogged-password-lost.html'
+                }
+            }
+        ]
+    };
 
 
 
 
-	// Handle the logged part (when user sign in successfully)
-	var loggedState = {
-		id : "root-logged",
-		// We load base structure : logo in top-left corner, and two part : menu and content
-		include : {
-			css  : "resource/css/root-logged.css",
-			html : "resource/html/root-logged.html"
-		},
+    // Handle the logged part (when user sign in successfully)
+    var loggedState = {
+        id:    'root-logged',
+        entry: '#page-container',
+        type:  'replace',
 
-		// We define how to use html loaded (content is a DOM document)
-		load : __replaceById("page-container"),
+        // We load base structure : logo in top-left corner, and two part:
+        // menu and content
+        include: {
+            css:  'resource/css/root-logged.css',
+            html: 'resource/html/root-logged.html'
+        },
 
-		children : [
-			// Handling mail menu
-			{
-				id   : "mail-menu",
+        children: [
+            // Handling mail menu
+            {
+                id: 'mail-menu',
+                entry: '#root-logged-menu',
+                type:  'replace',
 
-				include : {
-					// you can define shared css, it will be loaded only once
-					css  : "resource/css/root-logged-menu.css",
-					html : "resource/html/root-logged-mail-menu.html"
-				},
+                include: {
+                    // you can define shared css, it will be loaded only once
+                    css:  'resource/css/root-logged-menu.css',
+                    html: 'resource/html/root-logged-mail-menu.html'
+                },
 
-				// We define how to use html loaded (content is a DOM document)
-				load : __replaceById("root-logged-menu"),
+                // Dummy loader : we provide a simple hack to prevent bug on
+                // reloading directly on inbox (not showing menu)
+                children: [
+                    {
+                        hash: 'mail-{{type : [a-zA-Z0-9]+}}-{{page : \\d+}}'
+                    }
+                ]
+            },
 
-				// Dummy loader : we provide a simple hack to prevent bug on reloading directly on inbox (not showing menu)
-				children : [
-					{
-						hash : "mail-{{type : [a-zA-Z0-9]+}}-{{page : \\d+}}"
-					}
-				]
-			},
+            // Handling mail content
+            {
+                id:    'mail-content',
+                title: 'AppStorm.JS - mail',
+                entry: '#root-logged-content',
+                type:  'replace',
 
-			// Handling mail content
-			{
-				id    : "mail-content",
-				title : "AppStorm.JS - mail",
+                // We separate content in two sub content : the left panel,
+                // and right panel
+                include: {
+                    css:  'resource/css/root-logged-mail-content.css',
+                    html: 'resource/html/root-logged-mail-content.html'
+                },
 
-				// We separate content in two sub content : the left panel, and right panel
-				include : {
-					css  : "resource/css/root-logged-mail-content.css",
-					html : "resource/html/root-logged-mail-content.html"
-				},
+                // We include mail content as children to not unload other
+                // stuff
+                children: [
+                    {
+                        id:    'mail-content-inbox-menu',
+                        hash:  'mail-inbox-{{page : \\d+}}',
+                        entry: '#root-logged-mail-content-left',
+                        type:  'replace',
 
-				// We define how to use html loaded (content is a DOM document)
-				load : __replaceById("root-logged-content"),
+                        // We separate content in two sub content : the
+                        // left panel, and right panel
+                        include: {
+                            html: 'resource/html/root-logged-mail-menu-inbox.html'
+                        }
+                    },
+                    {
+                        id:    'mail-content-trash-menu',
+                        hash:  'mail-trash-{{page : \\d+}}',
+                        entry: '#root-logged-mail-content-left',
+                        type:  'replace', 
 
-				// We include mail content as children to not unload other stuff
-				children : [
-					{
-						id : "mail-content-inbox-menu",
-						hash : "mail-inbox-{{page : \\d+}}",
+                        // We separate content in two sub content :
+                        // the left panel, and right panel
+                        include: {
+                            html: 'resource/html/root-logged-mail-menu-trash.html'
+                        }
+                    },
+                    {
+                        id:    'mail-content-box',
+                        hash:  'mail-{{type : [a-zA-Z0-9]+}}-{{page : \\d+}}',
+                        title: 'AppStorm.JS - {{hash : type}} page {{hash : page}}',
+                        entry: '#root-logged-mail-content-right',
+                        type:  'replace',
 
-						// We separate content in two sub content : the left panel, and right panel
-						include : {
-							html : "resource/html/root-logged-mail-menu-inbox.html"
-						},
+                        // We use the hashtag to generate custom url regarding hastag
+                        data: 'resource/data/{{hash : type}}{{hash : page}}.json',
 
-						// We apply data
-						load : __replaceById("root-logged-mail-content-left")
-					},
-					{
-						id : "mail-content-trash-menu",
-						hash : "mail-trash-{{page : \\d+}}",
+                        // We separate content in two sub content :
+                        // the left panel, and right panel
+                        include: {
+                            html: 'resource/html/root-logged-mail-content-{{hash : type}}.html'
+                        }
+                    }
+                ]
+            },
 
-						// We separate content in two sub content : the left panel, and right panel
-						include : {
-							html : "resource/html/root-logged-mail-menu-trash.html"
-						},
+            // Handling about menu
+            {
+                id:    'about-menu',
+                hash:  'about',
+                entry: '#root-logged-menu',
+                type:  'replace',
 
-						// We apply data
-						load : __replaceById("root-logged-mail-content-left")
-					},
-					{
-						id : "mail-content-box",
-						hash : "mail-{{type : [a-zA-Z0-9]+}}-{{page : \\d+}}",
-						title : "AppStorm.JS - {{hash : type}} page {{hash : page}}",
+                // We separate content in two sub content:
+                // the left panel, and right panel
+                include: {
+                    // you can define shared css, it will be loaded only once
+                    css:  'resource/css/root-logged-menu.css',
+                    html: 'resource/html/root-logged-about-menu.html'
+                }
+            },
 
-						// We use the hashtag to generate custom url regarding hastag
-						data : "resource/data/{{hash : type}}{{hash : page}}.json",
+            // Handling about content
+            {
+                id:    'about-content',
+                hash:  'about',
+                title: 'AppStorm.JS - about',
+                entry: '#root-logged-content',
+                type:  'replace',
 
-						// We separate content in two sub content : the left panel, and right panel
-						include : {
-							html : "resource/html/root-logged-mail-content-{{hash : type}}.html"
-						},
-
-						// We apply data
-						load : __replaceById("root-logged-mail-content-right")
-					}
-				]
-			},
-
-			// Handling about menu
-			{
-				id   : "about-menu",
-				hash : "about",
-
-				// We separate content in two sub content : the left panel, and right panel
-				include : {
-					// you can define shared css, it will be loaded only once
-					css  : "resource/css/root-logged-menu.css",
-					html : "resource/html/root-logged-about-menu.html"
-				},
-
-				// We define how to use html loaded (content is a DOM document)
-				load : __replaceById("root-logged-menu")
-			},
-
-			// Handling about content
-			{
-				id    : "about-content",
-				hash  : "about",
-				title : "AppStorm.JS - about",
-
-				include : {
-					css  : "resource/css/root-logged-about-content.css",
-					html : "resource/html/root-logged-about-content.html"
-				},
-
-				// We define how to use html loaded (content is a DOM document)
-				load : __replaceById("root-logged-content")
-			}
-		]
-	};
+                include: {
+                    css:  'resource/css/root-logged-about-content.css',
+                    html: 'resource/html/root-logged-about-content.html'
+                }
+            }
+        ]
+    };
 
 
-	// This state is a "side" state : manually loaded by user, it does provide resource to create new email structure
-	// outside any "hashtag" change/event
-	var actionNewEmailState = {
-		id : "root-new-email",
+    // This state is a 'side' state : manually loaded by user,
+    // it does provide resource to create new email structure
+    // outside any 'hashtag' change/event
+    var actionNewEmailState = {
+        id:    'root-new-email',
+        entry: 'body',
+        type:  'append',
 
-		include : {
-			// We does create a basic structure for handling multiple "add email"
-			css  : "resource/css/root-logged-root-new-email.css",
-			html : "resource/html/root-logged-root-new-email.html"
-		},
+        include: {
+            // We does create a basic structure for handling
+            // multiple 'add email'
+            css:  'resource/css/root-logged-root-new-email.css',
+            html: 'resource/html/root-logged-root-new-email.html'
+        },
 
-		// We define how to use html loaded (content is a DOM document)
-		load : function(content) {
-			a.page.template.append(document.body, content);
-		},
+        children: {
+            id:    'new-email',
+            entry: '#root-new-email',
+            type:  'append',
 
-		children : [
-			{
-				id : "new-email",
+            include: {
+                html: 'resource/html/root-logged-new-email.html'
+            },
 
-				include : {
-					html : "resource/html/root-logged-new-email.html"
-				},
-
-				load : function(content) {
-					a.page.template.append(document.getElementById("root-new-email"), content);
-				},
-
-				unload : function(result) {
-					document.getElementById("root-new-email").innerHTML = "";
-					result.done();
-				}
-			}
-		]
-	};
+            unload: function() {
+                a.dom.id('root-new-email').empty();
+            }
+        }
+    };
 
 
 
 
-	// Finally we add elements to system
-	a.state.add(notLoggedState);
-	a.state.add(loggedState);
-	a.state.add(actionNewEmailState);
+    // Finally we add elements to system
+    a.state.add(notLoggedState);
+    a.state.add(loggedState);
+    a.state.add(actionNewEmailState);
 })();
