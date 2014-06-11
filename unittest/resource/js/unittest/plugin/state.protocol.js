315c0b6ab0598acd3e57fbd0c02457be0e9153ec
Charles VILLETTE <villette.charles@gmail.com>
2014-06-11 16:38:59 +0200

    Adding protocol unit test with multiple input.

--- unittest/resource/js/unittest/plugin/state.protocol.js
+++ unittest/resource/js/unittest/plugin/state.protocol.js
@@ -44,6 +44,37 @@ test('a.state.protocol-url', function() {
 });
 
 
+// Simple test url does work
+test('a.state.protocol-url-multi', function() {
+    var simple = {
+        hash: [
+            'superhash',
+            'second'
+        ]
+    };
+
+    var prefix = {
+        hash: [
+            'url://anotherone',
+            'url://anotherone2'
+        ]
+    };
+
+    var none = {
+
+    };
+
+    // They must all response that url is the good choice
+    var url = a.state.protocol.get('url');
+
+    strictEqual(url.fn(simple, 0), 'superhash');
+    strictEqual(url.fn(simple, 1), 'second');
+    strictEqual(url.fn(prefix, 0), 'anotherone');
+    strictEqual(url.fn(prefix, 1), 'anotherone2');
+    strictEqual(url.fn(none, 0), null);
+});
+
+
 // Test the specific uri protocol
 test('a.state.protocol-uri', function() {
 
@@ -108,4 +139,82 @@ test('a.state.protocol-uri', function() {
     };
 
     strictEqual(uri.fn(complex, 0), 'superb/superuri');
+});
+
+
+// Test the specific uri protocol
+test('a.state.protocol-uri-multiple', function() {
+
+    // First test
+    var simple = {
+        hash: [
+            'uri://superuri',
+            'uri://superuri2'
+        ],
+
+        parent: {
+            hash: [
+                'uri://ok',
+                'uri://ok2'
+            ]
+        }
+    };
+
+    var uri = a.state.protocol.get('uri');
+
+    strictEqual(uri.fn(simple, 0), 'ok/superuri');
+    strictEqual(uri.fn(simple, 1), 'ok/superuri2');
+
+
+    // Second test
+    var lessSimple = {
+        hash: [
+            'uri://superuri',
+            'uri://superuri2'
+        ],
+
+        parent: {
+            hash: [
+                'uri://ok',
+                'uri://ok2'
+            ],
+
+            parent: {
+                hash: [
+                    'uri://another',
+                    'uri://another2'
+                ]
+            }
+        }
+    };
+
+    strictEqual(uri.fn(lessSimple, 0), 'another/ok/superuri');
+    strictEqual(uri.fn(lessSimple, 1), 'another/ok/superuri2');
+
+
+    // Third test
+    var complex = {
+        hash: [
+            'uri://superuri',
+            'uri://superuri2'
+        ],
+
+        parent: {
+            // This one is in url, it should stop here
+            hash: [
+                'superb',
+                'superb2'
+            ],
+
+            parent: {
+                hash: [
+                    'uri://if-you-see-it-its-problem',
+                    'uri://if-you-see-it-its-problem2'
+                ]
+            }
+        }
+    };
+
+    strictEqual(uri.fn(complex, 0), 'superb/superuri');
+    strictEqual(uri.fn(complex, 1), 'superb/superuri2');
 });
