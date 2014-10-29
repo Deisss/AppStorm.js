688b26efdbaf33494a744333bd388b8daa101026
Charles VILLETTE <villette.charles@gmail.com>
2014-10-29 14:57:57 +0100

    Update acl unit test with new QUnit.

--- unittest/resource/js/unittest/core/acl.js
+++ unittest/resource/js/unittest/core/acl.js
@@ -1,6 +1,6 @@
 // Unit test for a.acl
 
-module('core/acl.js', {
+QUnit.module('core/acl.js', {
     teardown: function() {
         a.acl.unbindAll('change');
         a.message.unbindAll('a.acl.change');
@@ -12,72 +12,76 @@ module('core/acl.js', {
 
 
 // Test current role get and set
-test('a.acl.currentRole', function() {
+QUnit.test('a.acl.currentRole', function(assert) {
+    expect(2);
     a.acl.setCurrentRole('super-something');
-    strictEqual(a.acl.getCurrentRole(), 'super-something', 'Test role 1');
+    assert.strictEqual(a.acl.getCurrentRole(), 'super-something', 'Test role 1');
     a.acl.setCurrentRole('super');
-    strictEqual(a.acl.getCurrentRole(), 'super', 'Test role 2');
+    assert.strictEqual(a.acl.getCurrentRole(), 'super', 'Test role 2');
 });
 
 // Test roleList get and set
-test('a.acl.roleList', function() {
+QUnit.test('a.acl.roleList', function(assert) {
+    expect(2);
     a.acl.setRoleList(['a', 'b', 'c']);
-    strictEqual(a.acl.getRoleList().join(','), 'a,b,c', 'Test list');
+    assert.strictEqual(a.acl.getRoleList().join(','), 'a,b,c', 'Test list');
     a.acl.setRoleList(['a', 'z', 'k']);
-    strictEqual(a.acl.getRoleList().join(','), 'a,z,k', 'Test list 2');
+    assert.strictEqual(a.acl.getRoleList().join(','), 'a,z,k', 'Test list 2');
 });
 
 // Test allowed
-test('a.acl.allowed', function() {
+QUnit.test('a.acl.allowed', function(assert) {
+    expect(12);
     a.acl.setRoleList(['a', 'b', 'c']);
     a.acl.setCurrentRole('b');
 
     // Test allowed
-    strictEqual(a.acl.isAllowed('a', 'a'), true);
-    strictEqual(a.acl.isAllowed('a', 'b'), true);
-    strictEqual(a.acl.isAllowed('a', 'c'), true);
-    strictEqual(a.acl.isAllowed('b', 'a'), false);
-    strictEqual(a.acl.isAllowed('b', 'b'), true);
-    strictEqual(a.acl.isAllowed('b', 'c'), true);
-    strictEqual(a.acl.isAllowed('c', 'a'), false);
-    strictEqual(a.acl.isAllowed('c', 'b'), false);
-    strictEqual(a.acl.isAllowed('c', 'c'), true);
+    assert.strictEqual(a.acl.isAllowed('a', 'a'), true);
+    assert.strictEqual(a.acl.isAllowed('a', 'b'), true);
+    assert.strictEqual(a.acl.isAllowed('a', 'c'), true);
+    assert.strictEqual(a.acl.isAllowed('b', 'a'), false);
+    assert.strictEqual(a.acl.isAllowed('b', 'b'), true);
+    assert.strictEqual(a.acl.isAllowed('b', 'c'), true);
+    assert.strictEqual(a.acl.isAllowed('c', 'a'), false);
+    assert.strictEqual(a.acl.isAllowed('c', 'b'), false);
+    assert.strictEqual(a.acl.isAllowed('c', 'c'), true);
 
     // Finally test default role (current setted)
-    strictEqual(a.acl.isAllowed('a'), true);
-    strictEqual(a.acl.isAllowed('b'), true);
-    strictEqual(a.acl.isAllowed('c'), false);
+    assert.strictEqual(a.acl.isAllowed('a'), true);
+    assert.strictEqual(a.acl.isAllowed('b'), true);
+    assert.strictEqual(a.acl.isAllowed('c'), false);
 });
 
 // Test refused
-test('a.acl.refused', function() {
+QUnit.test('a.acl.refused', function(assert) {
+    expect(12);
     a.acl.setRoleList(['a', 'b', 'c']);
     a.acl.setCurrentRole('b');
 
     // Test refused
-    strictEqual(a.acl.isRefused('a', 'a'), false);
-    strictEqual(a.acl.isRefused('a', 'b'), false);
-    strictEqual(a.acl.isRefused('a', 'c'), false);
-    strictEqual(a.acl.isRefused('b', 'a'), true);
-    strictEqual(a.acl.isRefused('b', 'b'), false);
-    strictEqual(a.acl.isRefused('b', 'c'), false);
-    strictEqual(a.acl.isRefused('c', 'a'), true);
-    strictEqual(a.acl.isRefused('c', 'b'), true);
-    strictEqual(a.acl.isRefused('c', 'c'), false);
+    assert.strictEqual(a.acl.isRefused('a', 'a'), false);
+    assert.strictEqual(a.acl.isRefused('a', 'b'), false);
+    assert.strictEqual(a.acl.isRefused('a', 'c'), false);
+    assert.strictEqual(a.acl.isRefused('b', 'a'), true);
+    assert.strictEqual(a.acl.isRefused('b', 'b'), false);
+    assert.strictEqual(a.acl.isRefused('b', 'c'), false);
+    assert.strictEqual(a.acl.isRefused('c', 'a'), true);
+    assert.strictEqual(a.acl.isRefused('c', 'b'), true);
+    assert.strictEqual(a.acl.isRefused('c', 'c'), false);
 
     // Finally test default role (current setted)
-    strictEqual(a.acl.isRefused('a'), false);
-    strictEqual(a.acl.isRefused('b'), false);
-    strictEqual(a.acl.isRefused('c'), true);
+    assert.strictEqual(a.acl.isRefused('a'), false);
+    assert.strictEqual(a.acl.isRefused('b'), false);
+    assert.strictEqual(a.acl.isRefused('c'), true);
 });
 
 
 // Test event based is well performed
-asyncTest('a.acl-event', function() {
+QUnit.asyncTest('a.acl-event', function(assert) {
     expect(2);
 
     function testEvent(role) {
-        strictEqual(role, 'role-event-based');
+        assert.strictEqual(role, 'role-event-based');
     };
 
     a.message.bind('a.acl.change', testEvent);
@@ -85,5 +89,5 @@ asyncTest('a.acl-event', function() {
 
     a.acl.setCurrentRole('role-event-based');
 
-    setTimeout(start, 100);
+    setTimeout(start, 150);
 });
