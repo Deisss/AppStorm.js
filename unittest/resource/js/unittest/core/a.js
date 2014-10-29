b70705b5ba551791a08ef72c9caf92280cadf256
Charles VILLETTE <villette.charles@gmail.com>
2014-10-29 14:52:44 +0100

    Rework a.js with new QUnit recommandation.

--- unittest/resource/js/unittest/core/a.js
+++ unittest/resource/js/unittest/core/a.js
@@ -1,6 +1,6 @@
 // Unit test for a (the tiny part at beginning only)
 
-module('a.js', {
+QUnit.module('a.js', {
     teardown: function() {
         hashtag('');
         a.setDefaultAjaxOptions({});
@@ -11,46 +11,52 @@ module('a.js', {
 
 
 // Testing a found url...
-test('a.url', function() {
-    ok(a.url !== '', 'Test url not empty');
+QUnit.test('a.url', function(assert) {
+    expect(1);
+    assert.ok(a.url !== '', 'Test url not empty');
 });
 
 // Testing isNone
-test('a.isNone', function() {
-    strictEqual(a.isNone(null), true, 'Test null');
-    strictEqual(a.isNone(undefined), true, 'Test undefined');
-    strictEqual(a.isNone(''), false, 'Test empty string');
-    strictEqual(a.isNone({}), false, 'Test empty object');
-    strictEqual(a.isNone(0), false, 'Test 0 value');
+QUnit.test('a.isNone', function(assert) {
+    expect(5);
+    assert.strictEqual(a.isNone(null), true, 'Test null');
+    assert.strictEqual(a.isNone(undefined), true, 'Test undefined');
+    assert.strictEqual(a.isNone(''), false, 'Test empty string');
+    assert.strictEqual(a.isNone({}), false, 'Test empty object');
+    assert.strictEqual(a.isNone(0), false, 'Test 0 value');
 });
 
 // Testing a.isTrueObject
-test('a.isTrueObject', function() {
-    strictEqual(a.isTrueObject(null), false, 'Test null value');
-    strictEqual(a.isTrueObject(undefined), false, 'Test undefined value');
-    strictEqual(a.isTrueObject(function() {}), false, 'Test function value');
-    strictEqual(a.isTrueObject({}), true, 'Test object value');
+QUnit.test('a.isTrueObject', function(assert) {
+    expect(4);
+    assert.strictEqual(a.isTrueObject(null), false, 'Test null value');
+    assert.strictEqual(a.isTrueObject(undefined), false, 'Test undefined value');
+    assert.strictEqual(a.isTrueObject(function() {}), false, 'Test function value');
+    assert.strictEqual(a.isTrueObject({}), true, 'Test object value');
 });
 
-test('a.scope', function() {
+QUnit.test('a.scope', function(assert) {
+    expect(1);
     var scope = {
         test: 'ok'
     };
 
     var fct = a.scope(function() {
-        strictEqual(this.test, 'ok', 'Test scope');
+        assert.strictEqual(this.test, 'ok', 'Test scope');
     }, scope);
 
     fct();
 });
 
-test('a.trim', function() {
-    strictEqual(a.trim('     something is good       '), 'something is good');
-    strictEqual(a.trim('	with tab'), 'with tab');
+QUnit.test('a.trim', function(assert) {
+    expect(2);
+    assert.strictEqual(a.trim('     something is good       '), 'something is good');
+    assert.strictEqual(a.trim('	with tab'), 'with tab');
 });
 
-test('a.firstLetterUppercase', function() {
-    strictEqual(a.firstLetterUppercase('something', 'is'), 'isSomething');
-    strictEqual(a.firstLetterUppercase('something'), 'Something');
-    strictEqual(a.firstLetterUppercase('this is long'), 'This is long');
+QUnit.test('a.firstLetterUppercase', function(assert) {
+    expect(3);
+    assert.strictEqual(a.firstLetterUppercase('something', 'is'), 'isSomething');
+    assert.strictEqual(a.firstLetterUppercase('something'), 'Something');
+    assert.strictEqual(a.firstLetterUppercase('this is long'), 'This is long');
 });
