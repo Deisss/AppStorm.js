707e6754bd33610d4405d705fbb4f1117f56e905
Charles VILLETTE <villette.charles@gmail.com>
2014-10-29 16:26:32 +0100

    Make timer.js updated for new QUnit rules.

--- unittest/resource/js/unittest/core/timer.js
+++ unittest/resource/js/unittest/core/timer.js
@@ -1,6 +1,6 @@
 // Unit test for a.timer
 
-module('core/timer.js', {
+QUnit.module('core/timer.js', {
     setup: function() {
         a.timer.clear();
     },
@@ -13,12 +13,14 @@ module('core/timer.js', {
 
 
 // Test timer tick system
-asyncTest('a.timer.tick', function() {
+QUnit.asyncTest('a.timer.tick', function(assert) {
+    assert.expect(5);
+
     // We don't use expect as it can vary between 4 and 5 tick...
     var tickCount = 0;
 
     var fct = function() {
-        strictEqual(true, true, 'tick');
+        assert.strictEqual(true, true, 'tick');
         tickCount++;
     };
     a.message.bind('a.timer.tick', fct);
@@ -29,16 +31,18 @@ asyncTest('a.timer.tick', function() {
         // The test can run between 4 and 5 tick (as setTimeout is not precise)
         if(tickCount !== 4 && tickCount !== 5) {
             // In this case, a problem occurs
-            ok(1==2, 'Test fail, it has to be between 4 and 5 asserts. ' +
+            assert.ok(1==2, 'Test fail, it has to be between 4 and 5 asserts. ' +
                 'But instead it raise ' + tickCount + ' tests');
         }
 
-        start();
+        QUnit.start();
     }, 200);
 });
 
 // Test add function
-asyncTest('a.timer.add-get', function() {
+QUnit.asyncTest('a.timer.add-get', function(assert) {
+    assert.expect(6);
+
     // We don't use expect as it can vary between 5 and 6 tick...
     var tickCount = 3;
 
@@ -46,51 +50,51 @@ asyncTest('a.timer.add-get', function() {
     var idNull = a.timer.add(null, null, 50);
 
     var elementNull = a.timer.get(idNull);
-    strictEqual(elementNull.fct, null);
-    strictEqual(elementNull.scope, null);
+    assert.strictEqual(elementNull.fct, null);
+    assert.strictEqual(elementNull.scope, null);
 
     var idElement = a.timer.add(function() {
-        strictEqual(true, true, 'function tick');
+        assert.strictEqual(true, true, 'function tick');
         tickCount++;
     }, null, 100);
 
     var elementElement = a.timer.get(idElement);
-    strictEqual(elementElement.timeout, 100);
+    assert.strictEqual(elementElement.timeout, 100);
 
     setTimeout(function() {
         // The test can run between 5 and 6 tick (as setTimeout is not precise)
         if(tickCount !== 5 && tickCount !== 6) {
             // In this case, a problem occurs
-            ok(1==2, 'Test fail, it has to be between 5 and 6 asserts. ' +
+            assert.ok(1==2, 'Test fail, it has to be between 5 and 6 asserts. ' +
                 'But instead it raise ' + tickCount + ' tests');
         }
 
-        start();
+        QUnit.start();
     }, 270);
 });
 
 // Test once function
-asyncTest('a.timer.once', function() {
-    expect(1);
+QUnit.asyncTest('a.timer.once', function(assert) {
+    assert.expect(1);
 
     a.timer.once(function() {
-        strictEqual(true, true, 'Test raise once');
+        assert.strictEqual(true, true, 'Test raise once');
     }, null, 50);
 
-    setTimeout(start, 250);
+    setTimeout(QUnit.start, 250);
 });
 
 // Test remove function
-asyncTest('a.timer.remove', function() {
-    expect(0);
+QUnit.asyncTest('a.timer.remove', function(assert) {
+    assert.expect(0);
 
     a.timer.clear();
 
     var id = a.timer.add(function() {
-        strictEqual(true, true, 'Test raise once');
+        assert.strictEqual(true, true, 'Test raise once');
     }, null, 50);
 
     a.timer.remove(id);
 
-    setTimeout(start, 250);
+    setTimeout(QUnit.start, 250);
 });
