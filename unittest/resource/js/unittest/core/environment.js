551affc18a3a180de64ab18aaacb0b7cae17bf86
Charles VILLETTE <villette.charles@gmail.com>
2014-10-29 15:11:40 +0100

    Make environment.js compatible with new QUnit rules.

--- unittest/resource/js/unittest/core/environment.js
+++ unittest/resource/js/unittest/core/environment.js
@@ -1,6 +1,6 @@
 // Unit test for a.environment
 
-module('core/environment.js', {
+QUnit.module('core/environment.js', {
     teardown: function() {
         a.environment.clear();
     }
@@ -8,38 +8,42 @@ module('core/environment.js', {
 
 
 // Testing environment get/set and remove key from it
-test('a.environment.get-set-remove', function() {
+QUnit.test('a.environment.get-set-remove', function(assert) {
+    expect(5);
+
     var env = a.environment;
-    strictEqual(env.get('unittest'), null, 'Testing value before set');
+    assert.strictEqual(env.get('unittest'), null, 'Testing value before set');
 
     env.set('unittest', true);
-    strictEqual(env.get('unittest'), true, 'Testing value after set');
-    strictEqual(a.mem.get('app.environment.unittest'), true,
+    assert.strictEqual(env.get('unittest'), true, 'Testing value after set');
+    assert.strictEqual(a.mem.get('app.environment.unittest'), true,
                                                 'Test mem value after set');
 
     env.set('unittest', false);
-    strictEqual(env.get('unittest'), false, 'Testing updating value');
+    assert.strictEqual(env.get('unittest'), false, 'Testing updating value');
 
     env.remove('unittest');
-    strictEqual(env.get('unittest'), null, 'Testing value after remove');
+    assert.strictEqual(env.get('unittest'), null, 'Testing value after remove');
 });
 
 // Testing environment clear all key
-test('a.environment.clear', function() {
+QUnit.test('a.environment.clear', function(assert) {
+    expect(6);
+
     var env = a.environment;
 
     env.set('unittest1', 'ok');
     env.set('unittest4', 'ok');
 
-    strictEqual(env.get('unittest1'), 'ok', 'Testing value before clear');
-    strictEqual(env.get('unittest4'), 'ok', 'Testing value before clear');
-    strictEqual(a.mem.get('app.environment.unittest1'), 'ok',
+    assert.strictEqual(env.get('unittest1'), 'ok', 'Testing value before clear');
+    assert.strictEqual(env.get('unittest4'), 'ok', 'Testing value before clear');
+    assert.strictEqual(a.mem.get('app.environment.unittest1'), 'ok',
                                                 'Testing value before clear');
 
     env.clear();
 
-    strictEqual(env.get('unittest1'), null, 'Testing value after clear');
-    strictEqual(env.get('unittest4'), null, 'Testing value after clear');
-    strictEqual(a.mem.get('app.environment.unittest1'), null,
+    assert.strictEqual(env.get('unittest1'), null, 'Testing value after clear');
+    assert.strictEqual(env.get('unittest4'), null, 'Testing value after clear');
+    assert.strictEqual(a.mem.get('app.environment.unittest1'), null,
                                                 'Testing value before clear');
 });
