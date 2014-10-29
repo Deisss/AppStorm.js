fbd28a7f43d3be0e8c7fef22fa43c404ee43262d
Charles VILLETTE <villette.charles@gmail.com>
2014-10-29 16:24:03 +0100

    Making route.js now compatible with new QUnit rules.

--- unittest/resource/js/unittest/core/route.js
+++ unittest/resource/js/unittest/core/route.js
@@ -1,11 +1,13 @@
 // Unit test for a.route
 
-module('core/route.js', {
+QUnit.module('core/route.js', {
     setup: function() {
         hashtag('');
+        QAppStorm.clear();
     },
     teardown: function() {
         hashtag('');
+        QAppStorm.clear();
     }
 });
 
@@ -13,112 +15,120 @@ module('core/route.js', {
 
 
 // Test entering route system
-asyncTest('a.route.enter', function() {
-    expect(1);
+QUnit.asyncTest('a.route.enter', function(assert) {
+    assert.expect(1);
 
     // Dummy function to test entering route element
     function checkRoute(hash) {
-        strictEqual(hash, 'unittest-route1', 'Test entering');
+        QAppStorm.pop();
+        assert.strictEqual(hash, 'unittest-route1', 'Test entering');
     };
 
     // Binding function to route
     a.route.bind('unittest-route1', checkRoute);
 
-    chain('unittest-route1', function() {
-        hashtag('unittest-noroute1');
+    QAppStorm.chain({
+        hash: 'unittest-route1',
+        expect: 0
+    }, {
+        hash: 'unittest-noroute1',
+        expect: 1,
+        callback: function(chain) {
+            a.route.unbind('unittest-route1', checkRoute);
+            chain.next();
+        }
     });
-
-    chain('unittest-noroute1', function() {
-        a.route.unbind('unittest-route1', checkRoute);
-        start();
-    }, 100);
-
-    hashtag('unittest-route1');
 });
 
 // Test leaving route system
-asyncTest('a.route.leave', function() {
-    expect(1);
+QUnit.asyncTest('a.route.leave', function(assert) {
+    assert.expect(1);
 
     // Dummy function to test leaving route element
     function checkRoute(hash) {
-        strictEqual(hash, 'unittest-route2', 'Test leaving');
+        QAppStorm.pop();
+        assert.strictEqual(hash, 'unittest-route2', 'Test leaving');
     };
 
     // Binding function to route
     a.route.bind('unittest-route2', checkRoute, 'leave');
 
-    chain('unittest-route2', function() {
-        hashtag('unittest-noroute2');
+    QAppStorm.chain({
+        hash: 'unittest-route2',
+        expect: 0
+    }, {
+        hash: 'unittest-noroute2',
+        expect: 1,
+        callback: function(chain) {
+            a.route.unbind('unittest-route2', checkRoute);
+            chain.next();
+        }
     });
-
-    chain('unittest-noroute2', function() {
-        a.route.unbind('unittest-route2', checkRoute, 'leave');
-        start();
-    }, 100);
-
-    // Starting system
-    hashtag('unittest-route2');
 });
 
 // Test entering - otherwise system
-asyncTest('a.route.enter-otherwise', function() {
-    expect(2);
+QUnit.asyncTest('a.route.enter-otherwise', function(assert) {
+    assert.expect(1);
 
     // Dummy function to test entering route element
     function checkOtherwise() {
-        strictEqual(1, 1, 'Test otherwise enter');
+        QAppStorm.pop();
+        assert.strictEqual(1, 1, 'Test otherwise enter');
     };
 
     // Binding function to route
     a.route.otherwise(checkOtherwise);
 
-    chain('unittest-route-otherwise1', function() {
-        hashtag('unittest-route-nootherwise1');
+    QAppStorm.chain({
+        hash: 'unittest-route-otherwise1',
+        expect: 0
+    }, {
+        hash: 'unittest-route-nootherwise1',
+        expect: 1,
+        callback: function(chain) {
+            a.route.otherwise(null);
+            chain.next();
+        }
     });
-
-    chain('unittest-route-nootherwise1', function() {
-       a.route.otherwise(null);
-        start();
-    }, 100);
-
-    hashtag('unittest-route-otherwise1');
 });
 
 // Test leaving - otherwise system
-asyncTest('a.route.leaving-otherwise', function() {
-    expect(2);
+QUnit.asyncTest('a.route.leaving-otherwise', function(assert) {
+    assert.expect(1);
 
     // Dummy function to test leaving route element
     function checkOtherwise() {
-        strictEqual(1, 1, 'Test otherwise leaving');
+        QAppStorm.pop();
+        assert.strictEqual(1, 1, 'Test otherwise leaving');
     };
 
     // Binding function to route
     a.route.otherwise(checkOtherwise, 'leave');
 
-    chain('unittest-route-otherwise2', function() {
-        hashtag('unittest-route-nootherwise2');
+
+    QAppStorm.chain({
+        hash: 'unittest-route-otherwise2',
+        expect: 0
+    }, {
+        hash: 'unittest-route-nootherwise2',
+        expect: 1,
+        callback: function(chain) {
+            a.route.otherwise(null, 'leave');
+            chain.next();
+        }
     });
-
-    chain('unittest-route-nootherwise2', function() {
-        a.route.otherwise(null, 'leave');
-        start();
-    }, 100);
-
-    hashtag('unittest-route-otherwise2');
 });
 
 
-asyncTest('a.route.fake', function() {
-    expect(1);
+QUnit.asyncTest('a.route.fake', function(assert) {
+    assert.expect(1);
 
 
     // Dummy function to test entering route element
     function checkRoute(hash) {
-        strictEqual(hash, 'unittest-route3', 'Test entering');
+        assert.strictEqual(hash, 'unittest-route3', 'Test entering');
         a.route.unbind('unittest-route3', checkRoute);
-        start();
+        QUnit.start();
     };
 
     // Binding function to route
