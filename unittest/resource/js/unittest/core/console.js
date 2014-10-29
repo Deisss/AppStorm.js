878ceb201a122a94c1fefae41558100f2c16e7a2
Charles VILLETTE <villette.charles@gmail.com>
2014-10-29 15:04:09 +0100

    Unit test console.js is now compatible with new QUnit rules.

--- unittest/resource/js/unittest/core/console.js
+++ unittest/resource/js/unittest/core/console.js
@@ -1,6 +1,6 @@
 // Unit test for a.console
 
-module('core/console.js', {
+QUnit.module('core/console.js', {
     setup: function() {
         a.console.clear();
     },
@@ -13,66 +13,76 @@ module('core/console.js', {
 
 
 // Testing console 'log' mode
-test('a.console.log', function() {
+QUnit.test('a.console.log', function(assert) {
+    expect(4);
+
     a.console.log('one log', null, false);
     a.console.log('second log', null, false);
     a.console.log('third log', null, false);
 
     var trace = a.console.trace();
 
-    strictEqual(trace['log'].length, 3, 'Testing log size');
-    strictEqual(trace['log'][0], 'one log', 'Testing log trace');
-    strictEqual(trace['log'][1], 'second log', 'Testing log trace');
-    strictEqual(trace['log'][2], 'third log', 'Testing log trace');
+    assert.strictEqual(trace['log'].length, 3, 'Testing log size');
+    assert.strictEqual(trace['log'][0], 'one log', 'Testing log trace');
+    assert.strictEqual(trace['log'][1], 'second log', 'Testing log trace');
+    assert.strictEqual(trace['log'][2], 'third log', 'Testing log trace');
 });
 
 // Testing console 'info' mode
-test('a.console.info', function() {
+QUnit.test('a.console.info', function(assert) {
+    expect(4);
+
     a.console.info('one information', null, false);
     a.console.info('second information', null, false);
     a.console.info('third information', null, false);
 
     var trace = a.console.trace();
 
-    strictEqual(trace['info'].length, 3, 'Testing information size');
-    strictEqual(trace['info'][0], 'one information',
+    assert.strictEqual(trace['info'].length, 3, 'Testing information size');
+    assert.strictEqual(trace['info'][0], 'one information',
                                     'Testing information trace');
-    strictEqual(trace['info'][1], 'second information',
+    assert.strictEqual(trace['info'][1], 'second information',
                                     'Testing information trace');
-    strictEqual(trace['info'][2], 'third information',
+    assert.strictEqual(trace['info'][2], 'third information',
                                     'Testing information trace');
 });
 
 // Testing console 'warning' mode
-test('a.console.warn', function() {
+QUnit.test('a.console.warn', function(assert) {
+    expect(4);
+
     a.console.warn('one warning', null, false);
     a.console.warn('second warning', null, false);
     a.console.warn('third warning', null, false);
 
     var trace = a.console.trace();
 
-    strictEqual(trace['warn'].length, 3, 'Testing warning size');
-    strictEqual(trace['warn'][0], 'one warning', 'Testing warning trace');
-    strictEqual(trace['warn'][1], 'second warning', 'Testing warning trace');
-    strictEqual(trace['warn'][2], 'third warning', 'Testing warning trace');
+    assert.strictEqual(trace['warn'].length, 3, 'Testing warning size');
+    assert.strictEqual(trace['warn'][0], 'one warning', 'Testing warning trace');
+    assert.strictEqual(trace['warn'][1], 'second warning', 'Testing warning trace');
+    assert.strictEqual(trace['warn'][2], 'third warning', 'Testing warning trace');
 });
 
 // Testing console 'error' mode
-test('a.console.error', function() {
+QUnit.test('a.console.error', function(assert) {
+    expect(4);
+
     a.console.error('one error', null, false);
     a.console.error('second error', null, false);
     a.console.error('third error', null, false);
 
     var trace = a.console.trace();
 
-    strictEqual(trace['error'].length, 3, 'Testing error size');
-    strictEqual(trace['error'][0], 'one error', 'Testing error trace');
-    strictEqual(trace['error'][1], 'second error', 'Testing error trace');
-    strictEqual(trace['error'][2], 'third error', 'Testing error trace');
+    assert.strictEqual(trace['error'].length, 3, 'Testing error size');
+    assert.strictEqual(trace['error'][0], 'one error', 'Testing error trace');
+    assert.strictEqual(trace['error'][1], 'second error', 'Testing error trace');
+    assert.strictEqual(trace['error'][2], 'third error', 'Testing error trace');
 });
 
 // Testing trace (console keep data...)
-test('a.console.trace', function() {
+QUnit.test('a.console.trace', function(assert) {
+    expect(5);
+
     a.console.log('ok', null, false);
     a.console.log('ok2', null, false);
     a.console.log('ok3', null, false);
@@ -82,10 +92,10 @@ test('a.console.trace', function() {
     var log = trace['log'],
         warn = trace['warn'];
 
-    strictEqual(log[0], 'ok', 'Testing trace content');
-    strictEqual(log[1], 'ok2', 'Testing trace content');
-    strictEqual(log[2], 'ok3', 'Testing trace content');
-    strictEqual(warn[0], 'ok', 'Testing trace content');
+    assert.strictEqual(log[0], 'ok', 'Testing trace content');
+    assert.strictEqual(log[1], 'ok2', 'Testing trace content');
+    assert.strictEqual(log[2], 'ok3', 'Testing trace content');
+    assert.strictEqual(warn[0], 'ok', 'Testing trace content');
 
     // Now we send more than 2000 request on log,
     // we check the trace remove too old data
@@ -94,11 +104,13 @@ test('a.console.trace', function() {
     }
 
     var fullTrace = a.console.trace();
-    strictEqual(fullTrace['log'].length, 2000, 'Testing full stack');
+    assert.strictEqual(fullTrace['log'].length, 2000, 'Testing full stack');
 });
 
 // Testing trace (console keep data...)
-test('a.console.trace-type', function() {
+QUnit.test('a.console.trace-type', function(assert) {
+    expect(4);
+
     a.console.log('ok', null, false);
     a.console.log('ok2', null, false);
     a.console.log('ok3', null, false);
@@ -106,9 +118,9 @@ test('a.console.trace-type', function() {
 
     var log = a.console.trace('log');
 
-    strictEqual(log[0], 'ok', 'Testing trace content');
-    strictEqual(log[1], 'ok2', 'Testing trace content');
-    strictEqual(log[2], 'ok3', 'Testing trace content');
+    assert.strictEqual(log[0], 'ok', 'Testing trace content');
+    assert.strictEqual(log[1], 'ok2', 'Testing trace content');
+    assert.strictEqual(log[2], 'ok3', 'Testing trace content');
 
     // Now we send more than 2000 request on log,
     // we check the trace remove too old data
@@ -117,23 +129,25 @@ test('a.console.trace-type', function() {
     }
 
     var fullLog = a.console.trace('log');
-    strictEqual(fullLog.length, 2000, 'Testing full stack');
+    assert.strictEqual(fullLog.length, 2000, 'Testing full stack');
 });
 
 
 // Testing console clearing trace system
-test('a.console.clear', function() {
+QUnit.test('a.console.clear', function(assert) {
+    expect(4);
+
     a.console.log('ok', null, false);
     a.console.warn('ok', null, false);
 
     var trace = a.console.trace();
 
-    strictEqual(trace['log'].length, 1, 'Testing log before clear');
-    strictEqual(trace['warn'].length, 1, 'Testing warning before clear');
+    assert.strictEqual(trace['log'].length, 1, 'Testing log before clear');
+    assert.strictEqual(trace['warn'].length, 1, 'Testing warning before clear');
 
     a.console.clear();
     var newTrace = a.console.trace();
 
-    strictEqual(newTrace['log'].length, 0, 'Testing log after clear');
-    strictEqual(newTrace['warn'].length, 0, 'Testing warning after clear');
+    assert.strictEqual(newTrace['log'].length, 0, 'Testing log after clear');
+    assert.strictEqual(newTrace['warn'].length, 0, 'Testing warning after clear');
 });
