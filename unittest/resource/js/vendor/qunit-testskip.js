d46e1cd2ed69f87ff7c97cfcf94dd4d151bfa557
Charles VILLETTE <villette.charles@gmail.com>
2014-10-29 17:23:16 +0100

    Rework to make it working again with new QUnit.

--- unittest/resource/js/vendor/qunit-testskip.js
+++ unittest/resource/js/vendor/qunit-testskip.js
@@ -1,11 +1,18 @@
 // From : http://stackoverflow.com/questions/13748129/skipping-a-test-in-qunit
 QUnit.testSkip = function() {
-	QUnit.test(arguments[0] + ' (SKIPPED)', function() {
-		expect(0);
-		var li = document.getElementById(QUnit.config.current.id);
-		QUnit.done(function() {
-			li.style.background = '#FFFF99';
-		});
+	QUnit.test(arguments[0] + ' (SKIPPED)', function(assert) {
+		assert.expect(0);
+
+        // Get the current unit test, wich is an ID number (integer)
+        var id = QUnit.config.current.testNumber;
+
+        // Get the corresponding LI element to the given test number
+        var li = document.getElementById('qunit-test-output' + id);
+        QUnit.done(function() {
+            if(li) {
+                li.style.background = '#FFFF99';
+            }
+        });
 	});
 };
 testSkip = QUnit.testSkip;
