d46116e35784e9587df414544202177133c48aa4
Charles VILLETTE <villette.charles@gmail.com>
2014-11-30 02:28:57 +0100

    Adding ajax before and after function

--- appstorm/a.js
+++ appstorm/a.js
@@ -281,6 +281,62 @@ a.getTemplateAjaxOptions = function(name) {
     return a.mem.get('app.ajax.template.' + name);
 };
 
+/**
+ * Set a before action to perform on every ajax request using it
+ *
+ * @method setAjaxBefore
+ *
+ * @param name {String}                     The name to use for recall
+ * @param fct {Function | null}             The linked function to use
+*/
+a.setAjaxBefore = function(name, fct) {
+    if(name && a.isFunction(fct)) {
+        a.mem.set('app.ajax.before.' + name, fct);
+    }
+};
+
+/**
+ * Get an existing ajax before function, or null if nothing is found
+ *
+ * @method getAjaxBefore
+ *
+ * @param name {String}                     The name previously stored using
+ *                                          setAjaxBefore
+ * @return {Function | null}                The function if found, null in
+ *                                          other cases
+*/
+a.getAjaxBefore = function(name) {
+    return a.mem.get('app.ajax.before.' + name) || null;
+};
+
+/**
+ * Set an after action to perform on every ajax request using it
+ *
+ * @method setAjaxAfter
+ *
+ * @param name {String}                     The name to use for recall
+ * @param fct {Function | null}             The linked function to use
+*/
+a.setAjaxAfter = function(name, fct) {
+    if(name && a.isFunction(fct)) {
+        a.mem.set('app.ajax.after.' + name, fct);
+    }
+};
+
+/**
+ * Get an existing ajax after function, or null if nothing is found
+ *
+ * @method getAjaxAfter
+ *
+ * @param name {String}                     The name previously stored using
+ *                                          setAjaxAfter
+ * @return {Function | null}                The function if found, null in
+ *                                          other cases
+*/
+a.getAjaxAfter = function(name) {
+    return a.mem.get('app.ajax.after.' + name) || null;
+};
+
 /*
  * Check AppStorm.JS source url
 */
