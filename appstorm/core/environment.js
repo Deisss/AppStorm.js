e9fa217076caa93d7604e114fff979a7f06da397
Deisss <charles.villette@gmail.com>
2015-04-19 11:15:33 +0200

    Dummy changes

--- appstorm/core/environment.js
+++ appstorm/core/environment.js
@@ -52,13 +52,16 @@ if(a.isString(a.url) && a.url.length > 0) {
 ------------------------------
 */
 (function() {
+    // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
     var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-        // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
-    var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    // At least Safari 3+: "[object HTMLElementConstructor]"
     var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-        // At least Safari 3+: "[object HTMLElementConstructor]"
-    var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
-    var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+    // Chrome 1+
+    var isChrome = !!window.chrome && !isOpera;
+    // At least IE6
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
 
     var browser = 'other';
     if (isOpera) {
