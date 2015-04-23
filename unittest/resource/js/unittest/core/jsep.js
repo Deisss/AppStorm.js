7bbda81e08de6dd5dc7e702976a30c92d5e825c2
Charles VILLETTE <villette.charles@gmail.com>
2015-04-23 11:04:17 +0200

    Updating jsep unit test.

--- unittest/resource/js/unittest/core/jsep.js
+++ unittest/resource/js/unittest/core/jsep.js
@@ -78,3 +78,142 @@ QUnit.test('a.jsep.custom-operator', function(assert) {
     assert.strictEqual(result2.variables.length, 1);
     assert.strictEqual(result2.variables[0], 'a');
 });
+
+
+// Custom operators do not affect others
+QUnit.test('a.jsep.custom-operator-parallel', function(assert) {
+    assert.expect(2);
+
+    var tree = a.jsep.parse('1 + 1');
+    var parser1 = a.jsep.interpreter('qunit.jsep.co-parallel1', true, true,
+            true),
+        parser2 = a.jsep.interpreter('qunit.jsep.co-parallel2', true, true,
+            true);
+
+    parser2.binaryOperators.set('+', function (left, right) {
+        return left - right;
+    });
+
+    var result1 = parser1.evaluate(tree),
+        result2 = parser2.evaluate(tree);
+
+    assert.strictEqual(result1.result, 2, 'Test computing 1');
+    assert.strictEqual(result2.result, 0, 'Test computing 2');
+});
+
+
+// Test nest functions call
+QUnit.test('a.jsep.function', function(assert) {
+    assert.expect(2);
+
+    var tree = a.jsep.parse('a(b(12))');
+    var parser = a.jsep.interpreter('qunit.jsep.function', true, true, true);
+
+    var result = parser.evaluate(tree, {
+        a: function(value) {
+            return value - 4;
+        },
+        b: function(value) {
+            return value * 2;
+        }
+    });
+
+    assert.strictEqual(result.result, 20, 'Test computing');
+    assert.strictEqual(result.variables.length, 0);
+});
+
+
+// Using object
+QUnit.test('a.jsep.object', function(assert) {
+    assert.expect(4);
+
+    var tree = a.jsep.parse('"ok " + a.b[c] + " " + 4 + ok + func(2, 3)');
+    var parser = a.jsep.interpreter('qunit.jsep.object', true, true, true);
+
+    var result = parser.evaluate(tree, {
+        func: function(a, b) {
+            return a + ' yatta ' + b;
+        },
+        a: {
+            b: {
+                c: 'member'
+            }
+        },
+        ok: 'hello'
+    });
+
+    assert.strictEqual(result.result, 'ok member 4hello2 yatta 3', 'concat');
+    assert.strictEqual(result.variables.length, 2, 'Test variables length');
+    assert.strictEqual(result.variables[0], 'a', 'variable 1');
+    assert.strictEqual(result.variables[1], 'ok', 'variable 2');
+});
+
+
+// Logical operator not defined raise error on console
+QUnit.test('a.jsep.not-defined-logical', function(assert) {
+    assert.expect(2);
+
+    var tree = a.jsep.parse('1 && 1');
+    var parser = a.jsep.interpreter('qunit.jsep.not-defined-logical', true,
+            false, true);
+
+    var result = parser.evaluate(tree),
+        trace = a.console.trace('error');
+
+    assert.strictEqual(trace[0].source,
+            'a.jsep.interpreter.qunit.jsep.not-defined-logical','test source');
+    assert.ok(trace[0].args[0].indexOf('Unknow logical operator ```&&```') >0);
+});
+
+
+// Binary operator not defined raise error on console
+QUnit.test('a.jsep.not-defined-binary', function(assert) {
+    assert.expect(2);
+
+    var tree = a.jsep.parse('1 + 1');
+    var parser = a.jsep.interpreter('qunit.jsep.not-defined-binary', false,
+            true, true);
+
+    var result = parser.evaluate(tree),
+        trace = a.console.trace('error');
+
+    assert.strictEqual(trace[0].source,
+            'a.jsep.interpreter.qunit.jsep.not-defined-binary','test source');
+    assert.ok(trace[0].args[0].indexOf('Unknow binary operator ```+```') >0);
+});
+
+
+// Unary operator not defined raise error on console
+QUnit.test('a.jsep.not-defined-unary', function(assert) {
+    assert.expect(2);
+
+    var tree = a.jsep.parse('-a');
+    var parser = a.jsep.interpreter('qunit.jsep.not-defined-unary', true, true,
+            false);
+
+    var result = parser.evaluate(tree),
+        trace = a.console.trace('error');
+
+    assert.strictEqual(trace[0].source,
+            'a.jsep.interpreter.qunit.jsep.not-defined-unary','test source');
+    assert.ok(trace[0].args[0].indexOf('Unknow unary operator ```-```') >0);
+});
+
+
+// Test an indentifier does not exist in scope
+// NOTE: identifier cannot put something on console, as it can easily
+// raise wrong error like this (for example with object members identifier
+// will be called...)
+// So we just check the system does not crash...
+QUnit.test('a.jsep.not-defined-identifier', function(assert) {
+    assert.expect(2);
+
+    var tree = a.jsep.parse('a');
+    var parser = a.jsep.interpreter('qunit.jsep.not-defined-identifier', true,
+            true, true);
+
+    var result = parser.evaluate(tree);
+
+    assert.strictEqual(result.result, 'a', 'Test string return');
+    assert.strictEqual(result.variables.length, 0);
+});
